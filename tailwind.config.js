3a5d8fe47dc0362eb637f7325d7c48b7535d06c5
Wairegi Githu <githubenard16@gmail.com>
2024-11-04 10:02:53 +0300

    correct index.html

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,3 +7,15 @@ module.exports = {
   plugins: [],
 }
 
+// tailwind.config.js
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        'fleur-de-leah': ['"Fleur De Leah"', 'serif'], // Add Fleur De Leah font
+        'delius': ['"Delius"', 'cursive'],            // Add Delius font
+      },
+    },
+  },
+  plugins: [],
+};
